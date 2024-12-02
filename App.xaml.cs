41fe2ab908c873e328998ef3c3365dea9a289e7a
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-12-02 16:23:05 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using System;
+using System.Windows;
+
+namespace SerilogWPFExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Serilog konfigurieren: Logs in die Konsole und eine Datei schreiben
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day) // Log-Datei wird täglich rotiert
+                .CreateLogger();
+
+            // Beispiel-Log-Nachricht, um Serilog zu testen
+            Log.Information("Anwendung gestartet.");
+
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.CloseAndFlush(); // Logger beim Beenden freigeben
+            base.OnExit(e);
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using System;
+using System.Windows;
+
+namespace SerilogWPFExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Log-Nachricht erstellen
+            Log.Information("Der Button wurde geklickt!");
+
+            // Log-Level Beispiel
+            Log.Debug("Debug-Nachricht: Button wurde geklickt.");
+            Log.Warning("Warnung: Button wurde geklickt.");
+            Log.Error("Fehler: Etwas ist schiefgelaufen.");
+            Log.Fatal("Fataler Fehler: Anwendung könnte abstürzen.");
+        }
+    }
+}
